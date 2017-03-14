65799499f15cc1120db80066345b6139808cf73d
Andrea Tosato <dossobuono@gmail.com>
2017-03-14 21:25:59 +0100

    aggiunta JSNLog

--- ASP.NET Core/LogClientSideMVC/Startup.cs
+++ ASP.NET Core/LogClientSideMVC/Startup.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-//using JSNLog;
+using JSNLog;
 
 namespace LogClientSideMVC
 {
@@ -15,25 +15,22 @@ namespace LogClientSideMVC
     {
         public Startup(IHostingEnvironment env)
         {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
-                .AddEnvironmentVariables();
-            Configuration = builder.Build();
-
+            var builder = new ConfigurationBuilder();
             if (env.IsDevelopment())
             {
                 builder.AddApplicationInsightsSettings(developerMode: true);
             }
+            builder.SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
         }
 
         public IConfigurationRoot Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
+        
         public void ConfigureServices(IServiceCollection services)
         {
-            // Add framework services.
             services.AddMvc();
             services.AddApplicationInsightsTelemetry(Configuration);
         }
@@ -53,8 +50,9 @@ namespace LogClientSideMVC
             {
                 app.UseExceptionHandler("/Home/Error");
             }
-            //var jsnlogConfiguration = new JsnlogConfiguration();
-            //app.UseJSNLog(new LoggingAdapter(loggerFactory), jsnlogConfiguration);
+
+            var jsnlogConfiguration = new JsnlogConfiguration();
+            app.UseJSNLog(new LoggingAdapter(loggerFactory), jsnlogConfiguration);
 
             app.UseStaticFiles();
 
